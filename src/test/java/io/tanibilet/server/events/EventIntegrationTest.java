3d34202e5202f78efe2d7b386f3f0e3db54c7ecf
Jakub Szydełko <kubarroo248@interia.pl>
2025-06-03 21:52:55 +0200

    Adds EventIntegrationTest GetEvent tests

--- src/test/java/io/tanibilet/server/events/EventIntegrationTest.java
+++ src/test/java/io/tanibilet/server/events/EventIntegrationTest.java
@@ -111,4 +111,115 @@ public class EventIntegrationTest {
         verify(eventRepository, times(0)).save(any(EventEntity.class));
     }
 
+    @Test
+    void testGetEvents()
+    {
+        //Arrange
+        EventEntity eventEntity2 = new EventEntity(
+                null,
+                "Test2",
+                ZonedDateTime.now().plusDays(1),
+                ZonedDateTime.now().plusDays(3),
+                "Kraków",
+                25.0,
+                100,
+                user.userId(),
+                "Test2 description",
+                false,
+                EventType.CONFERENCE,
+                new HashSet<>()
+        );
+
+        List<EventEntity> events = List.of(
+                eventEntity,
+                eventEntity2
+        );
+
+        List<EventDto> expectedEvents = List.of(
+                EventDto.fromEventEntity(eventEntity),
+                EventDto.fromEventEntity(eventEntity2)
+        );
+
+        when(eventRepository.findAll()).thenReturn(events);
+
+        //Act
+        List<EventDto> foundEvents = eventController.getEvents();
+
+        //Assert
+        assertNotNull(foundEvents);
+        assertEquals(2, foundEvents.size());
+        assertEquals(expectedEvents, foundEvents);
+
+        verify(eventRepository, times(1)).findAll();
+    }
+
+    @Test
+    void testGetEventForUser()
+    {
+        //Arrange
+        EventEntity eventEntity2 = new EventEntity(
+                null,
+                "Test2",
+                ZonedDateTime.now().plusDays(1),
+                ZonedDateTime.now().plusDays(3),
+                "Kraków",
+                25.0,
+                100,
+                user.userId(),
+                "Test2 description",
+                false,
+                EventType.CONFERENCE,
+                new HashSet<>()
+        );
+        List<EventEntity> events = List.of(
+                eventEntity,
+                eventEntity2
+        );
+
+        when(eventRepository.findAllByOwnerUserId(user.userId())).thenReturn(List.of(eventEntity));
+
+        //Act
+        List<EventDto> result = eventController.getEventForUser(user);
+
+        //Assert
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertEquals(List.of(EventDto.fromEventEntity(eventEntity)), result);
+
+        verify(eventRepository, times(1)).findAllByOwnerUserId(user.userId());
+    }
+
+    @Test
+    void testGetEventByIdSuccessfully()
+    {
+        //Arrange
+        when(eventRepository.findById(1L)).thenReturn(Optional.of(eventEntity));
+        EventDto expectedDto = EventDto.fromEventEntity(eventEntity);
+
+        //Act
+        ResponseEntity<EventDto> event = eventController.getEventById(1L);
+
+        //Assert
+        assertEquals(HttpStatus.OK, event.getStatusCode());
+        assertNotNull(event.getBody());
+        assertEquals(expectedDto, event.getBody());
+
+        verify(eventRepository, times(1)).findById(1L);
+    }
+
+    @Test
+    void testGetEventByIdStatusNotFound()
+    {
+        //Arrange
+        when(eventRepository.findById(1L)).thenReturn(Optional.empty());
+
+        //Act
+        ResponseEntity<EventDto> event = eventController.getEventById(1L);
+
+        //Assert
+        assertEquals(HttpStatus.NOT_FOUND, event.getStatusCode());
+
+        verify(eventRepository, times(1)).findById(1L);
+    }
+
 }
