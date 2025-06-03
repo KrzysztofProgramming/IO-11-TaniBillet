18028c68c7510212231fa83c6dfaf9d9f626c166
Jakub Szydełko <kubarroo248@interia.pl>
2025-06-03 21:18:46 +0200

    Adds EventIntegrationTest class

---
+++ src/test/java/io/tanibilet/server/events/EventIntegrationTest.java
@@ -0,0 +1,76 @@
+﻿package io.tanibilet.server.events;
+
+import io.tanibilet.server.auth.UserPrincipal;
+import io.tanibilet.server.events.dto.CreateEventDto;
+import io.tanibilet.server.events.entities.EventEntity;
+import io.tanibilet.server.events.entities.EventType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.time.ZonedDateTime;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
+public class EventIntegrationTest {
+
+    EventRepository eventRepository = mock(EventRepository.class);
+    private EventService eventService = new EventService(eventRepository);
+    private EventController eventController = new EventController(eventService);
+
+    private static UserPrincipal user;
+    private static CreateEventDto createEventDto;
+    private static EventEntity eventEntity;
+
+    @BeforeAll
+    static void SetUp()
+    {
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+        authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        authorities.add(new SimpleGrantedAuthority("ROLE_event_creator"));
+
+        user = new UserPrincipal(
+                "1",
+                "test@mail.com",
+                "TestUser",
+                true,
+                "John",
+                "Doe",
+                authorities
+        );
+
+        createEventDto = new CreateEventDto(
+                "Test",
+                ZonedDateTime.now().plusDays(1),
+                ZonedDateTime.now().plusDays(2),
+                "Kraków",
+                50.0,
+                200,
+                "Test description",
+                false,
+                EventType.CONCERT
+        );
+
+        eventEntity = new EventEntity(
+                null,
+                "Test",
+                ZonedDateTime.now().plusDays(1),
+                ZonedDateTime.now().plusDays(2),
+                "Kraków",
+                50.0,
+                200,
+                user.userId(),
+                "Test description",
+                false,
+                EventType.CONCERT,
+                new HashSet<>()
+        );
+    }
+
+}
